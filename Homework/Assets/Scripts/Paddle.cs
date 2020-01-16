c142d3e036988e89577ed55e44c9fa31f37a1840
Samuli Lehtonen <samulilehtonen33@gmail.com>
2020-01-16 09:06:22 +0200

    Tasks 1 and 2 on Pong

---
+++ Homework/Assets/Scripts/Paddle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    public float paddleSpeed = 10.0f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.name == "Paddle1")
+        {
+            if (Input.GetKey("w"))
+            {
+                transform.Translate(0, paddleSpeed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey("s"))
+            {
+                transform.Translate(0, -paddleSpeed * Time.deltaTime, 0);
+            }
+        }
+        if(gameObject.name == "Paddle2") 
+        {
+            if (Input.GetKey("up"))
+            {
+                transform.Translate(0, paddleSpeed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey("down"))
+            {
+                transform.Translate(0, -paddleSpeed * Time.deltaTime, 0);
+            }
+        }
+    }
+}
---
+++ Homework/Assets/Scripts/PongBall.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PongBall : MonoBehaviour
+
+{
+  
+    //Speed of the ball
+    public float pongBallSpeed = 10.0f;
+    //Starting location of the ball
+    PongBall pongBall;
+    Vector2 pongBallStart;
+    //Boolean if the game is on or not
+    private bool gameIsOn = false;
+    //Moving vector
+    Vector2 moving;
+    //place holder for points
+    int pointsBlue = 0;
+    int pointsRed = 0;
+    //text gameobjects
+    Text bluePointText;
+    Text redPointText;
+    
+    
+
+   // Start is called before the first frame update
+    void Start()
+    {
+        //Set moving direction: x:1, y:1 and add the coordinates to current position with normalized
+        moving = Vector2.one.normalized;
+        //set gameobjects
+        pongBall = gameObject.GetComponent<PongBall>();
+        
+        bluePointText = GameObject.Find("PlayerBluePoints").GetComponent<Text>();
+        redPointText = GameObject.Find("PlayerRedPoints").GetComponent<Text>();
+        //attach component
+        
+        //save start position
+        pongBallStart = pongBall.transform.position;
+        //Debug.Log(pongBallStart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Start game by pressing space
+        if (Input.GetKey("space"))
+        {
+            gameIsOn = true;
+        }
+
+        if (gameIsOn)
+        {
+            transform.Translate(moving * pongBallSpeed * Time.deltaTime);
+        }
+        
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+     
+        if (collision.gameObject.name == "NorthWall" || collision.gameObject.name == "SouthWall")
+        {
+            moving.y = -moving.y;
+        }
+     
+        if (collision.gameObject.name == "Paddle1" || collision.gameObject.name == "Paddle2")
+        {
+            moving.x = -moving.x;
+        }
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Paddle1Goal")
+        {
+            pongBall.transform.position = pongBallStart;
+            pointsBlue += 1;
+            gameIsOn = false;
+            bluePointText.text = "Blue Points: " + pointsBlue.ToString();
+        }
+        if (collision.gameObject.name == "Paddle2Goal")
+        {
+            pongBall.transform.position = pongBallStart;
+            pointsRed += 1;
+            gameIsOn = false;
+            redPointText.text = "Red Points: " + pointsRed.ToString();
+        }
+    }
+}
